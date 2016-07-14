9cb565b376283c530f6569c0675b0a645bc17597
tcarnaru <tud_underworld@yahoo.com>
2016-07-14 17:42:30 +0300

    Added most functionality

--- src/main/java/co/uk/endava/controller/HomeController.java
+++ src/main/java/co/uk/endava/controller/HomeController.java
@@ -1,6 +1,9 @@
 package co.uk.endava.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,5 +18,17 @@ public class HomeController {
         //new ModelAndView("Test","modelName", "model value");
         //new ModelAndView("Test",new HashMap<String, Object>());
         return new ModelAndView("welcome","message","Good morning!");
+    }
+    @RequestMapping(value="/InsertStuff", method= RequestMethod.GET)
+    public ModelAndView student() {
+        return new ModelAndView("LogIn", "command", new User());
+    }
+
+    @RequestMapping(value = "/addUser", method = RequestMethod.POST)
+    public String addStudent(@ModelAttribute("SpringWeb")User user,
+                             ModelMap model) {
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("password", user.getPassword());
+        return "result";
     }
 }
